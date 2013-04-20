3944278374e374dabc5828362f6bec37a9b9cc86
Colin Wren <colinwrenca@gmail.com>
2013-04-20 13:59:46 -0700

    fixed task spawn for windows

--- tasks/sails.js
+++ tasks/sails.js
@@ -7,21 +7,24 @@
 
 module.exports = function(grunt) {
 
-  var spawn = require('child_process').spawn;
-
   grunt.registerMultiTask('sails', 'Start a sails app with forever', function() {
 
     grunt.log.writeln('Starting sails server');
 
     // Start server and restart when files change
-    var sails = spawn('./node_modules/grunt-sails/node_modules/forever/bin/forever', ['-w', 'app.js']);
 
-    sails.stdout.on('data', function (data) {
-      console.log('' + data);
-    });
-
-    sails.stderr.on('data', function (data) {
-      console.log('' + data);
+    grunt.util.spawn({
+      cmd: './node_modules/grunt-sails/node_modules/forever/bin/forever',
+      args: ['-w', 'app.js'],
+      opts: {
+        stdio: 'inherit'
+      }
+    }, function (error, result) {
+      if (error) {
+        grunt.fail.fatal(result.stdout);
+      }
+      grunt.log.writeln(result.stdout);
+      done();
     });
   });
 };
