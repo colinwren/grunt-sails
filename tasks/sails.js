9af7fc3c4e731a40e22949394defb344c2906126
Colin Wren <colinwrenca@gmail.com>
2013-04-25 18:57:16 -0700

    Added options for configuring sails server environment

--- tasks/sails.js
+++ tasks/sails.js
@@ -9,13 +9,20 @@ module.exports = function(grunt) {
 
   grunt.registerMultiTask('sails', 'Start a sails app with forever', function() {
 
+    var args = ['-w', 'app.js'];
+    var options = this.options();
+
+    // Use options for specifying Sails server environment
+    if (options.development) args.push('--dev');
+    if (options.production) args.push('--prod');
+
     grunt.log.writeln('Starting sails server');
 
     // Start server and restart when files change
 
     grunt.util.spawn({
       cmd: './node_modules/grunt-sails/node_modules/forever/bin/forever',
-      args: ['-w', 'app.js'],
+      args: args,
       opts: {
         stdio: 'inherit'
       }
